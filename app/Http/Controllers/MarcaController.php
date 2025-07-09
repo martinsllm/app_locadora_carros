f2f0682de0ffcf3e28ed1dfa96f8c7132957ccf2
martinsllm <lucaslm2010@gmail.com>
2025-07-09 14:40:25 -0300

    Searching data via GET method

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -12,7 +12,8 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        //
+        $results = Marca::all();
+        return $results;
     }
 
     /**
@@ -29,7 +30,7 @@ class MarcaController extends Controller
      */
     public function show(Marca $marca)
     {
-        //
+        return $marca;
     }
 
     /**
