444357edd412831b341fd1e84c6020a96533b980
martinsllm <lucaslm2010@gmail.com>
2025-07-11 14:39:46 -0300

    Creating endpoints for table Model

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -4,15 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ModeloController extends Controller
 {
+    protected $modelo;
+
+    public function __construct(Modelo $modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return response()->json($this->modelo->all(), 200);
     }
 
     /**
@@ -20,30 +28,83 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $imagem = $request->file('imagem');
+        $imagem_urn = $imagem->store('imagens/modelos', 'public');
+
+        $result = $this->modelo->create([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+
+        return response()->json($result, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if ($modelo === null) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404);
+        }
+
+        return response()->json($modelo, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if ($modelo === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização: o recurso solicitado não existe.'], 404);
+        }
+
+        //Remove o arquivo antigo se for enviado um no request
+        if ($request->file('imagem')) {
+            Storage::disk('public')->delete($modelo->imagem);
+        }
+
+        $imagem = $request->file('imagem');
+        $imagem_urn = $imagem->store('imagens/modelos', 'public');
+
+        $modelo->update([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+
+        return response()->json($modelo, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if ($modelo === null) {
+            return response()->json(['erro' => 'Impossível realizar a exclusão: o recurso solicitado não existe.'], 404);
+        }
+
+        //Remove o arquivo se existir
+        Storage::disk('public')->delete($modelo->imagem);
+
+        $modelo->delete();
+        return response()->json(['msg' => 'Modelo removido com sucesso!'], 200);
     }
 }
---
+++ app/Http/Requests/ModeloRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ModeloRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'marca_id' => 'exists:marcas,id',
+            'nome' => 'required|unique:modelo,nome,' . $this->route('modelo'),
+            'imagem' => 'required|file|mimes:png,jpg,jpeg',
+            'numero_portas' => 'required|integer|digits_between:1,5',
+            'lugares' => 'required|integer|digits_between:1,20',
+            'air_bag' => 'required|boolean',
+            'abs' => 'required|boolean',
+        ];
+    }
+}
